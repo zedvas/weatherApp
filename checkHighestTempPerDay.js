50463eef904c51bbd56120327c44317245f474fe
zedvas <zahravasanji@hotmail.com>
2024-06-08 10:26:12 +0100

    fixed bug and redid highest temp func

--- checkHighestTempPerDay.js
+++ checkHighestTempPerDay.js
@@ -1,11 +1,21 @@
-function checkHighestTempPerDay (day, temp, obj) {
-    if (obj.hasOwnProperty(day)) {
-      if (temp > obj[day]) {obj[day] = temp}
+function checkHighestTempPerDay(weatherEntry, obj) {
+    // grab temp and day to compare
+
+    const date = new Date(weatherEntry.dt * 1000).getDay();
+    const temp = weatherEntry.main.temp;
+
+    if (!obj.hasOwnProperty(date)) {
+      obj[date] = weatherEntry;
+    } else {
+      if (temp > obj[date].main.temp) {
+        obj[date] = weatherEntry;
+      }
+
+      // console.log("yes exist");
+      // }
+
+      //run the function here and append all the data to new obj ot boil it down
     }
-    else {
-      obj[day] = temp
-    }
-    return obj;
   }
 
   export default checkHighestTempPerDay;
--- doSomethingWithWeather.js
+++ doSomethingWithWeather.js
@@ -1,6 +1,12 @@
 import createHTML from "./createHTML.js";
 import checkHighestTempPerDay from "./checkHighestTempPerDay.js";
 
+/*handle api result takes in result from fetch 
+loops through result and for each entry
+it checks against external object for highest highestTemp
+the obj gets returned
+loop over object, modify data to display as you wish and create html */
+
 function doSomethingWithWeather(result) {
   //retrieve location info
   const {
@@ -11,79 +17,60 @@ function doSomethingWithWeather(result) {
   createHTML(country, "p", root);
 
   //empty obj to hold highest temp per day
-  const highestTemp = {};
+  const highestTempPerDay = {};
 
   //retrieve weather info
   for (let i = 0; i < result.list.length; i++) {
-    const listObject = result.list[i];
-
-    //destructure result obj and grab relevant data
-    let {
-      dt: dateTimestamp,
-      main: { temp },
-      weather,
-    } = listObject;
-
-    let { icon, description } = weather[0];
-
-    //convert temp and add to highestTemp obj
-    temp = Math.round(temp - 273.15);
-
-    //format date
-    const days = [
-      "sunday",
-      "monday",
-      "tuesday",
-      "wednesday",
-      "thursday",
-      "friday",
-      "saturday",
-    ];
-    const months = [
-      "jan",
-      "feb",
-      "mar",
-      "apr",
-      "may",
-      "jun",
-      "jul",
-      "aug",
-      "sep",
-      "oct",
-      "nov",
-      "dec",
-    ];
-    let dateString = new Date(dateTimestamp * 1000);
-
-    const day = days[dateString.getDay()];
-    const date = dateString.getDate()
-const month = months[dateString.getMonth()];
-
-    dateString = `${day} ${date} ${month} ${dateString.getHours()}`;
-
-
-    //check date
-    //if date is nto in obj, append it as key and set value to temp
-    //if date is in obj, check if temp is higher than current ubj value
-    //if yes, replace tha value in obj with current temp
-    //if not, return?
-
-
-checkHighestTempPerDay(day, temp, highestTemp);
-
-    //create HTML
-    createHTML(dateString, "p", root);
-    createHTML(temp, "h1", root);
-    createHTML(description, "h2", root);
-    // decide what to do with icon
-    //append these to correct parents. decide where to declare parents.
-    //loop through
-  }
-  console.log(highestTemp)
+    const weatherEntry = result.list[i];
+console.log(weatherEntry.dt_txt, weatherEntry.main.temp)
+checkHighestTempPerDay(weatherEntry,highestTempPerDay)
+}  console.log(highestTempPerDay);
 
 }
-
 export default doSomethingWithWeather;
 
-/*
-reduce down object - what are theoptions?  */
+//then loop over new obj and do teh below.
+
+//destructure result obj and grab relevant data
+//     //convert temp and add to highestTemp obj
+//     temp = Math.round(temp - 273.15);
+
+//     //format date
+//     const days = [
+//       "sunday",
+//       "monday",
+//       "tuesday",
+//       "wednesday",
+//       "thursday",
+//       "friday",
+//       "saturday",
+//     ];
+//     const months = [
+//       "jan",
+//       "feb",
+//       "mar",
+//       "apr",
+//       "may",
+//       "jun",
+//       "jul",
+//       "aug",
+//       "sep",
+//       "oct",
+//       "nov",
+//       "dec",
+//     ];
+//     let dateString = new Date(dateTimestamp * 1000);
+
+//     const day = days[dateString.getDay()];
+//     const date = dateString.getDate()
+// const month = months[dateString.getMonth()];
+
+//     dateString = `${day} ${date} ${month} ${dateString.getHours()}`;
+
+//     //create HTML
+//     createHTML(dateString, "p", root);
+//     createHTML(temp, "h1", root);
+//     createHTML(description, "h2", root);
+//     // decide what to do with icon
+//     //append these to correct parents. decide where to declare parents.
+//     //loop through
