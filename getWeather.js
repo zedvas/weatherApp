4868bdd7ab391517f2fe9efbecf81a085112d3ad
zedvas <zahravasanji@hotmail.com>
2024-06-12 18:03:20 +0100

    incorporated geocoding api

--- getWeather.js
+++ getWeather.js
@@ -1,16 +1,30 @@
 import doSomethingWithWeather from "./doSomethingWithWeather.js";
+import { API_KEY } from "./secrets.js";
 
-async function getWeather(lat, lon) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=fd0964bf9dd0b68ad3c6c68136f8c483`;
+//refactor to make this also accept a search term,
+async function getWeather(lat, lon, searchTerm) {
+  //if search term was passed through, use the geocoding api to get co-ords
+  if (searchTerm) {
+    let result = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm},gb&limit=5&appid=${API_KEY}`
+    );
+    result = await result.json();
+    [lat, lon] = [result[0].lat, result[0].lon];
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+  //results[0].lat,lon
   try {
     let result = await fetch(url);
     result = await result.json();
+    console.log(result);
     doSomethingWithWeather(result);
   } catch (e) {
-    throw new Error(e);       //check if error handling is correct here
+    console.log(e);
   }
-
-  
 }
 
 export default getWeather;
+
+//http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm},gb&limit=5&appid=fd0964bf9dd0b68ad3c6c68136f8c483
--- index.js
+++ index.js
@@ -1,20 +1,39 @@
+import createHTML from "./createHTML.js";
 import getWeather from "./getWeather.js";
 
-const root = document.getElementById("root");
+const rootRef = document.getElementById("root");
+
+//create input field
+const inputRef = document.createElement("input");
+inputRef.type="text";
+inputRef.placeholder = "search a city here";
+const formRef = document.createElement("form");
+
+formRef.append(inputRef);
+rootRef.append(formRef)
+
+//add event listener to input
+let searchTerm, lat, lon;
+formRef.addEventListener("submit", e=> {
+e.preventDefault();
+searchTerm = e.target[0].value;
+console.log(searchTerm)
+getWeather(lat, lon, searchTerm);
+e.target[0].value = "";
+})
 
 //obtain geolocation from user
 const options = { enableHighAccuracy: true, timeout: 100000, maximumAge: 60000 };
 navigator.geolocation.getCurrentPosition(success, error, options);
 
-
 //handle response from geolocation and obtain user coords
-function success({ coords: { latitude: lat }, coords: { longitude: lon } }) {
+function success({ coords: { latitude: lat, longitude: lon } }) {
   //plug coords into fetch to weather api and extract relevant info;
-  getWeather(lat, lon);
+  getWeather(lat, lon, searchTerm);
 }
 
 function error(err) {
   console.log(err);
 }
 
-//set default values to 0,0?
+//might need to do something to make sure there's no race condition (?) between search and navigation
