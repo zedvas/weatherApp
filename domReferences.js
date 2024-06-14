ec5e6450a177362628365b2fba24da3ea4ac5f52
zedvas <zahravasanji@hotmail.com>
2024-06-14 20:53:52 +0100

    fixed display current date bug and removed unecessary files

--- domReferences.js
+++
@@ -1,4 +0,0 @@
-export const rootRef = document.getElementById("root");
-export const containerRef = document.getElementsByClassName("container")[0];
-export const linksContainerRef = document.getElementsByClassName("linksContainer")[0];
-export const infoContainerRef = document.getElementsByClassName("infoContainer")[0];
